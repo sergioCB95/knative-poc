dabf8a87c2908f44349f2084ad1b4bcf6e7fcbec
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2023-07-04 20:33:45 +0200

    adding core infra

---
+++ core/apply.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+kubectl apply -f https://github.com/knative/operator/releases/download/knative-v1.9.4/operator.yaml
+
---
+++ core/delete.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+kubectl delete -f https://github.com/knative/operator/releases/download/knative-v1.9.4/operator.yaml
+
